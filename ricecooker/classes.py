8661e2e031f962cbf0c78e6a0930ec1c2760c96a
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-09-13 12:10:38 -0700

    Added classes for topics, videos, audio, documents, and exercises

--- ricecooker/classes.py
+++ ricecooker/classes.py
@@ -8,10 +8,13 @@ def generate_uuid(name):
 
 class Channel:
     def __init__(self, channel_id, domain=None, title=None):
-        channel_uuid = generate_uuid(channel_id)
         self.domain = domain
-        self.channel_id = channel_uuid
+        self.channel_id = generate_uuid(channel_id)
         self.title = title
+        self.root = Topic(
+            id=self.channel_id,
+            title="root"
+        )
 
     def to_json(self):
         return {
@@ -19,3 +22,77 @@ class Channel:
             "title": self.title,
             "thumbnail": self.thumbnail,
         }
+
+def generate_content_id(domain, id):
+    return uuid.uuid5(domain, id)
+
+def generate_node_id(parent_id, content_id):
+    return uuid.uuid5(parent_id, content_id)
+
+class Node:
+    def __init__(self, id, title, description, author):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.author = author
+
+    def guess_content_kind(self):
+        pass
+
+    def to_json(self):
+        pass
+
+    def set_ids(self, domain, parent):
+        self.content_id = generate_content_id(domain, self.id)
+        self.node_id = generate_node_id(parent.id, self.content_id)
+
+
+# def propagate_content_ids(channeltree):
+#     assert isinstance(channeltree, Channel)
+
+
+# def set_ids(root):
+#     children = channeltree.children
+
+#     for child in children:
+#         child.content_id = uuid(channel.domain_id, child.id)
+
+#     propagate_content_ids(channeltree)
+
+class Topic(Node):
+    def __init__(self, id, title, description=None, author=None):
+        self.children = []
+        self.kind = constants.CK_TOPIC
+        super(Topic, self).__init__(id, title, description, author)
+
+class Video(Node):
+    default_preset = constants.FP_VIDEO_HIGH_RES
+    def __init__(self, id, title, author=None, description=None, transcode_to_lower_resolutions=False, derive_thumbnail=False):
+        self.transcode_to_lower_resolutions = transcode_to_lower_resolutions
+        self.derive_thumbnail = derive_thumbnail
+        self.kind = constants.CK_VIDEO
+        super(Video, self).__init__(id, title, description, author)
+
+    def derive_thumbnail(self):
+        pass
+
+    def transcode_to_lower_resolution(self):
+        pass
+
+class Audio(Node):
+    default_preset = constants.FP_AUDIO
+    def __init__(self, id, title, author=None, description=None):
+        self.kind = constants.CK_AUDIO
+        super(Audio, self).__init__(id, title, description, author)
+
+class Document(Node):
+    default_preset = constants.FP_DOCUMENT
+    def __init__(self, id, title, author=None, description=None):
+        self.kind = constants.CK_DOCUMENT
+        super(Document, self).__init__(id, title, description, author)
+
+class Exercise(Node):
+    default_preset = constants.FP_EXERCISE
+    def __init__(self, id, title, author=None, description=None):
+        self.kind = constants.CK_EXERCISE
+        super(Exercise, self).__init__(id, title, description, author)
--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,19 +1,26 @@
 import pytest
 import uuid
-from ricecooker.classes import Channel
+from fle_utils import constants
+from ricecooker.classes import *
 
+
+""" *********** CHANNEL FIXTURES *********** """
 @pytest.fixture
 def channel_id():
     return "sample-channel"
+
+@pytest.fixture
+def channel_domain():
+    return "learningequality.org"
 
 @pytest.fixture
 def channel_uuid(channel_id):
     return uuid.uuid3(uuid.NAMESPACE_DNS, uuid.uuid5(uuid.NAMESPACE_DNS, channel_id).hex).hex
 
 @pytest.fixture
-def channel_data(channel_id):
+def channel_data(channel_id, channel_domain):
     return {
-    	"domain" : "learningequality.org",
+    	"domain" : channel_domain,
         "channel_id" : channel_id,
         "title" : "Sample Channel",
     }
@@ -26,6 +33,136 @@ def channel(channel_data):
 		title=channel_data['title'],
 	)
 
+
+
+""" *********** TOPIC FIXTURES *********** """
+@pytest.fixture
+def topic_id():
+    return "topic-id"
+
+@pytest.fixture
+def topic_data(topic_id):
+    return {
+    	"title": "topic node test",
+    	"description": None,
+        "id" : topic_id,
+        "author": None,
+    }
+
+@pytest.fixture
+def topic(topic_data):
+    return Topic(
+		id=topic_data['id'],
+		description=topic_data['description'],
+		title=topic_data['title'],
+		author=topic_data['author']
+	)
+
+
+
+""" *********** VIDEO FIXTURES *********** """
+@pytest.fixture
+def video_id():
+    return "video-id"
+
+@pytest.fixture
+def video_data(video_id):
+    return {
+    	"title": "video node test",
+    	"description": None,
+        "id" : video_id,
+        "author": None,
+    }
+
+@pytest.fixture
+def video(video_data):
+    return Video(
+		id=video_data['id'],
+		description=video_data['description'],
+		title=video_data['title'],
+		author=video_data['author'],
+		transcode_to_lower_resolutions=True,
+		derive_thumbnail=True,
+	)
+
+
+
+""" *********** AUDIO FIXTURES *********** """
+@pytest.fixture
+def audio_id():
+    return "audio-id"
+
+@pytest.fixture
+def audio_data(audio_id):
+    return {
+    	"title": "audio node test",
+    	"description": None,
+        "id" : audio_id,
+        "author": None,
+    }
+
+@pytest.fixture
+def audio(audio_data):
+    return Audio(
+		id=audio_data['id'],
+		description=audio_data['description'],
+		title=audio_data['title'],
+		author=audio_data['author'],
+	)
+
+
+
+""" *********** DOCUMENT FIXTURES *********** """
+@pytest.fixture
+def document_id():
+    return "document-id"
+
+@pytest.fixture
+def document_data(document_id):
+    return {
+    	"title": "document node test",
+    	"description": None,
+        "id" : document_id,
+        "author": None,
+    }
+
+@pytest.fixture
+def document(document_data):
+    return Document(
+		id=document_data['id'],
+		description=document_data['description'],
+		title=document_data['title'],
+		author=document_data['author'],
+	)
+
+
+
+""" *********** EXERCISE FIXTURES *********** """
+@pytest.fixture
+def exercise_id():
+    return "exercise-id"
+
+@pytest.fixture
+def exercise_data(exercise_id):
+    return {
+    	"title": "exercise node test",
+    	"description": None,
+        "id" : exercise_id,
+        "author": None,
+    }
+
+@pytest.fixture
+def exercise(exercise_data):
+    return Exercise(
+		id=exercise_data['id'],
+		description=exercise_data['description'],
+		title=exercise_data['title'],
+		author=exercise_data['author'],
+	)
+
+
+
+""" *********** CHANNEL TESTS *********** """
 def test_channel_created(channel):
 	assert channel is not None
 
@@ -33,3 +170,93 @@ def test_channel_data(channel, channel_data, channel_uuid):
 	assert channel.domain == channel_data['domain']
 	assert channel.title == channel_data['title']
 	assert channel.channel_id == channel_uuid
+
+def test_channel_root(channel):
+	assert channel.root is not None
+	assert channel.root.kind == constants.CK_TOPIC
+
+
+
+""" *********** TOPIC TESTS *********** """
+def test_topic_created(topic):
+	assert topic is not None
+
+def test_topic_data(topic, topic_data):
+	assert topic.id == topic_data['id']
+	assert topic.title == topic_data['title']
+	assert topic.description == topic_data['description']
+	assert topic.author == topic_data['author']
+	assert topic.kind == constants.CK_TOPIC
+
+def test_topic_ids(topic):
+	assert True
+
+
+
+""" *********** VIDEO TESTS *********** """
+def test_video_created(video):
+	assert video is not None
+
+def test_video_data(video, video_data):
+	assert video.id == video_data['id']
+	assert video.title == video_data['title']
+	assert video.description == video_data['description']
+	assert video.author == video_data['author']
+	assert video.kind == constants.CK_VIDEO
+
+def test_video_default_preset(video):
+	assert video.default_preset == constants.FP_VIDEO_HIGH_RES
+
+def test_video_derive_thumbnail(video):
+	assert True
+
+def test_video_transcode_to_lower_resolution(video):
+	assert True
+
+
+
+""" *********** AUDIO TESTS *********** """
+def test_audio_created(audio):
+	assert audio is not None
+
+def test_audio_data(audio, audio_data):
+	assert audio.id == audio_data['id']
+	assert audio.title == audio_data['title']
+	assert audio.description == audio_data['description']
+	assert audio.author == audio_data['author']
+	assert audio.kind == constants.CK_AUDIO
+
+def test_audio_default_preset(audio):
+	assert audio.default_preset == constants.FP_AUDIO
+
+
+
+""" *********** DOCUMENT TESTS *********** """
+def test_document_created(document):
+	assert document is not None
+
+def test_document_data(document, document_data):
+	assert document.id == document_data['id']
+	assert document.title == document_data['title']
+	assert document.description == document_data['description']
+	assert document.author == document_data['author']
+	assert document.kind == constants.CK_DOCUMENT
+
+def test_document_default_preset(document):
+	assert document.default_preset == constants.FP_DOCUMENT
+
+
+
+""" *********** EXERCISE TESTS *********** """
+def test_exercise_created(exercise):
+	assert exercise is not None
+
+def test_exercise_data(exercise, exercise_data):
+	assert exercise.id == exercise_data['id']
+	assert exercise.title == exercise_data['title']
+	assert exercise.description == exercise_data['description']
+	assert exercise.author == exercise_data['author']
+	assert exercise.kind == constants.CK_EXERCISE
+
+def test_exercise_default_preset(exercise):
+	assert exercise.default_preset == constants.FP_EXERCISE
