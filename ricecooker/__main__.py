5190e6676868c5eeb51aa1f7e53246f9487cdd7c
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-09-29 10:53:40 -0700

    added separation for debug and cc server

--- ricecooker/__main__.py
+++ ricecooker/__main__.py
@@ -1,4 +1,4 @@
-"""Usage: ricecooker uploadchannel [-hvqr] <file_path>
+"""Usage: ricecooker uploadchannel [-hvqr] <file_path> [--debug]
 
 Arguments:
   file_path        Path to file with channel data
@@ -13,9 +13,14 @@ Options:
 from ricecooker.commands import uploadchannel
 from docopt import docopt
 
+PRODUCTION_DOMAIN = "http://unicefcontentcuration.learningequality.org"
+DEBUG_DOMAIN = "127.0.0.1:8000"
+
 commands = ["uploadchannel"]
 
 if __name__ == '__main__':
     arguments = docopt(__doc__)
-
-    uploadchannel(arguments["<file_path>"], verbose=arguments["-v"])
+    domain = PRODUCTION_DOMAIN
+    if arguments["--debug"]:
+    	domain = DEBUG_DOMAIN
+    uploadchannel(arguments["<file_path>"], domain, verbose=arguments["-v"])
