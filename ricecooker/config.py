1caf1812596d14e9b41bfe0d63a6552e0688a3d2
Jordan Yoshihara <jayoshih@ucsd.edu>
2016-10-27 09:49:10 -0700

    Added authentication url

--- ricecooker/config.py
+++ ricecooker/config.py
@@ -8,6 +8,9 @@ PRODUCTION_DOMAIN = "https://contentworkshop.learningequality.org"
 # Domain for uploading to local machine
 DEBUG_DOMAIN = "http://127.0.0.1:8000"
 
+# URL for authenticating user on Kolibri Studio
+AUTHENTICATION_URL = "{domain}/api/internal/authenticate_user_internal"
+
 # URL for getting file diff
 FILE_DIFF_URL = "{domain}/api/internal/file_diff"
 
@@ -24,39 +27,46 @@ OPEN_CHANNEL_URL = "{domain}/open_channel/{invitation_id}/{channel_id}"
 STORAGE_DIRECTORY = "storage/"
 
 def get_storage_path(filename):
-	""" get_storage_path: returns path to storage directory for downloading content
+    """ get_storage_path: returns path to storage directory for downloading content
         Args: filename (str): Name of file to store
         Returns: string path to file
     """
-	return os.path.join(STORAGE_DIRECTORY, filename)
+    return os.path.join(STORAGE_DIRECTORY, filename)
+
+def authentication_url(domain):
+    """ authentication_url: returns url to login to Kolibri Studio
+        Args: domain (str): domain to log in
+        Returns: string url to authenticate_user_internal endpoint
+    """
+    return AUTHENTICATION_URL.format(domain=domain)
 
 def file_diff_url(domain):
-	""" file_diff_url: returns url to get file diff
+    """ file_diff_url: returns url to get file diff
         Args: domain (str): domain to get file diff from
         Returns: string url to file_diff endpoint
     """
-	return FILE_DIFF_URL.format(domain=domain)
+    return FILE_DIFF_URL.format(domain=domain)
 
 def file_upload_url(domain):
-	""" file_upload_url: returns url to upload files
+    """ file_upload_url: returns url to upload files
         Args: domain (str): domain to upload files to
         Returns: string url to file_upload endpoint
     """
-	return FILE_UPLOAD_URL.format(domain=domain)
+    return FILE_UPLOAD_URL.format(domain=domain)
 
 def create_channel_url(domain):
-	""" create_channel_url: returns url to create channel
+    """ create_channel_url: returns url to create channel
         Args: domain (str): domain to create channel on
         Returns: string url to create_channel endpoint
     """
-	return CREATE_CHANNEL_URL.format(domain=domain)
+    return CREATE_CHANNEL_URL.format(domain=domain)
 
 def open_channel_url(invitation, channel, domain):
-	""" open_channel_url: returns url to uploaded channel
+    """ open_channel_url: returns url to uploaded channel
         Args:
-        	invitation (str): invitation id to get editing access
-        	channel (str): channel id of uploaded channel
-        	domain (str): server where channel was created
+            invitation (str): invitation id to get editing access
+            channel (str): channel id of uploaded channel
+            domain (str): server where channel was created
         Returns: string url to open channel
     """
-	return OPEN_CHANNEL_URL.format(domain=domain, invitation_id=invitation, channel_id=channel)
+    return OPEN_CHANNEL_URL.format(domain=domain, invitation_id=invitation, channel_id=channel)
