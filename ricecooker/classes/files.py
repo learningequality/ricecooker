f2f1d719f3b84551da466d26712334a7952f8c13
Jordan Yoshihara <jordanyoshihara@gmail.com>
2017-01-27 13:06:04 -0800

    Update files.py

--- ricecooker/classes/files.py
+++ ricecooker/classes/files.py
@@ -103,7 +103,7 @@ def get_hash(filepath):
 def compress_video_file(filename, ffmpeg_settings):
     # Generate key for compressed file
     setting_list = ffmpeg_settings if ffmpeg_settings else {}
-    settings = " {}".format(str(sorted(setting_list.items()))) if ffmpeg_settings else " (default)"
+    settings = " {}".format(str(sorted(setting_list.items()))) if ffmpeg_settings else " (default compression)"
     key = "COMPRESSED: {0}{1}".format(filename, settings)
     if not config.UPDATE and FILECACHE.get(key):
         return FILECACHE.get(key).decode('utf-8')
