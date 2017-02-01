43c819132f3bcaa64fdf1cdf4cec51e6d20de024
Jordan Yoshihara <jayoshih@ucsd.edu>
2017-02-01 11:32:25 -0800

    Updated source_url as parameter

--- ricecooker/classes/files.py
+++ ricecooker/classes/files.py
@@ -147,12 +147,12 @@ class File(object):
     filename = None
     language = None
     assessment_item = None
-    source_url = None
 
-    def __init__(self, preset=None, language=None, default_ext=None):
+    def __init__(self, preset=None, language=None, default_ext=None, source_url=None):
         self.preset = preset
         self.language = language
         self.default_ext = default_ext or self.default_ext
+        self.source_url = source_url
 
     def validate(self):
         pass
@@ -192,7 +192,6 @@ class DownloadFile(File):
 
     def __init__(self, path, **kwargs):
         self.path = path.strip()
-        self.source_url = self.path
         super(DownloadFile, self).__init__(**kwargs)
 
     def validate(self):
