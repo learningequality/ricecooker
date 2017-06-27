9bce7759b770b50ebb36bf0b8b7bcc974038f615
Benjamin Bach <benjamin@overtag.dk>
2017-06-27 14:54:20 +0200

    Fail explicitely if Python version is too old

--- ricecooker/__init__.py
+++ ricecooker/__init__.py
@@ -3,3 +3,9 @@
 __author__ = 'Learning Equality'
 __email__ = 'jordan@learningequality.org'
 __version__ = '0.6.0'
+
+
+import sys
+
+if sys.version_info < (3, 4, 0):
+    raise RuntimeError("Ricecooker only supports Python 3.4+")
