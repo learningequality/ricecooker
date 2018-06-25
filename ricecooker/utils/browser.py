0679e6a1edf82acfe0ee1f8d7ae1ac1ccd550572
kollivier <kevino@theolliviers.com>
2018-06-25 15:30:27 -0700

    Make local server preview feature available as a command, and add an option for testing in a Kolibri iframe sandbox environment.

--- ricecooker/utils/browser.py
+++ ricecooker/utils/browser.py
@@ -1,8 +1,62 @@
-import os, urllib, posixpath, webbrowser
-from http.server import HTTPServer, BaseHTTPRequestHandler, SimpleHTTPRequestHandler
+import os
+import posixpath
+import sys
+import urllib
+import webbrowser
+
+from http.server import HTTPServer, HTTPStatus, SimpleHTTPRequestHandler
+
+SERVER = "127.0.0.1"
+PORT = 8282
 
 
-def preview_in_browser(directory, filename="index.html", port=8282):
+class IFrameServerRequestHandler(SimpleHTTPRequestHandler):
+    def do_OPTIONS(self):
+        self.send_access_control_headers()
+        self.send_response(200, "ok")
+
+    def send_access_control_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+
+    def do_GET(self):
+        path = self.translate_path(self.path)
+        print("path = {}".format(path))
+        ctype = self.guess_type(path)
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.send_error(HTTPStatus.NOT_FOUND, "File not found")
+            return None
+        try:
+            self.send_response(HTTPStatus.OK)
+            self.send_header("Content-type", ctype)
+            fs = os.fstat(f.fileno())
+            self.send_header("Content-Length", str(fs[6]))
+            self.send_header("Last-Modified", self.date_time_string(fs.st_mtime))
+            self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+            self.send_header("Pragma", "no-cache")
+            self.send_header("Expires", "0")
+
+            self.send_access_control_headers()
+
+            self.send_header("Content-Security-Policy",
+                             "default-src 'self' 'unsafe-inline' 'unsafe-eval' data: http://{}:{}".format(SERVER, PORT))
+            self.end_headers()
+        except:
+            f.close()
+            raise
+
+        if f:
+            try:
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()
+        return
+
+
+def preview_in_browser(directory, filename="index.html"):
 
     class RequestHandler(SimpleHTTPRequestHandler):
 
@@ -21,8 +75,32 @@ def preview_in_browser(directory, filename="index.html", port=8282):
                 path = os.path.join(path, word)
             return path
 
-    httpd = HTTPServer(("127.0.0.1", port), RequestHandler)
+    httpd = HTTPServer((SERVER, PORT), RequestHandler)
 
-    webbrowser.open("http://127.0.0.1:{}/{}".format(port, filename))
+    webbrowser.open("http://{}:{}/{}".format(SERVER, PORT, filename))
 
     httpd.serve_forever()
+
+def iframe_preview(filepath):
+
+    filename = "index.html"
+    directory = filepath
+    if not os.path.isdir(filepath):
+        filename = os.path.basename(filepath)
+        directory = os.path.dirname(filepath)
+
+    os.chdir(directory)
+    httpd = HTTPServer((SERVER, PORT), IFrameServerRequestHandler)
+
+    webbrowser.open("http://{}:{}/{}".format(SERVER, PORT, filename))
+
+    httpd.serve_forever()
+
+def load_server(path, iframe=False):
+    if iframe:
+        print("Starting local server and iframe sandbox...")
+        iframe_preview(path)
+
+    else:
+        print("Starting local server...")
+        preview_in_browser(path)
