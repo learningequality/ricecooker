b6663ab3ee1791d9b1fd3aee24bf0509a4cebe84
WenyuZhang1992 <wenyuzhang1992@gmail.com>
2020-07-18 16:41:58 -0700

    Add test for YouTube playlist

--- tests/test_youtube_cache.py
+++ tests/test_youtube_cache.py
@@ -1,8 +1,7 @@
 import os
 import pytest
 
-from ricecooker.utils.youtube_cache import YouTubeVideoCache 
-
+from ricecooker.utils.youtube_cache import YouTubeVideoCache, YouTubePlaylistCache
 
 
 """ *********** YouTube Cache FIXTURES *********** """
@@ -13,6 +12,11 @@ def youtube_video_cache():
     assert os.path.isdir(cache_dir), 'Incorrect directory path setting'
     return YouTubeVideoCache(video_id='DFZb2qBIrEw', alias='test-video', cache_dir=cache_dir)
 
+@pytest.fixture
+def youtube_playlist_cache():
+    cache_dir = os.path.join('tests', 'testcontent', 'youtubecache')
+    assert os.path.isdir(cache_dir), 'Incorrect directory path setting'
+    return YouTubePlaylistCache(playlist_id='PLOZioxrIwCv33zt5aFFjWqDoEMm55MVA9', alias='test-playlist', cache_dir=cache_dir)
 
 
 """ *********** YouTube Cache TESTS *********** """
@@ -21,3 +25,9 @@ def test_youtube_video_cache(youtube_video_cache):
     youtube_video_cache.get_video_info()
     video_cache_filepath = os.path.join('tests', 'testcontent', 'youtubecache', 'test-video.json')
     assert os.path.exists(video_cache_filepath)
+
+def test_youtube_playlist_cache(youtube_playlist_cache):
+    youtube_playlist_cache.get_playlist_info()
+    playlist_cache_filepath = os.path.join('tests', 'testcontent', 'youtubecache', 'test-playlist.json')
+    assert os.path.exists(playlist_cache_filepath)
+    
