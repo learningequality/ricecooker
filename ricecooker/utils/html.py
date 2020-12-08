0c13e74ee8cd720715679c361ffab259b5ffdb62
kollivier <kevino@theolliviers.com>
2020-12-08 13:42:34 -0800

    Ensure we only try to convert text files to Unicode, and rely on requests to determine the charset of the text.

--- ricecooker/utils/html.py
+++ ricecooker/utils/html.py
@@ -106,7 +106,13 @@ def download_file(url, destpath, filename=None, baseurl=None, subpath=None, midd
 
     # if there are any middleware callbacks, apply them to the content
     if middleware_callbacks:
-        content = content.decode()
+        if 'content-type' in response.headers:
+            type = response.headers['content-type'].split(';')[0]
+            # Rely on requests to convert bytes to unicode for us when it's a text file
+            # otherwise, we just use bytes
+            if type.startswith('text'):
+                content = response.text
+
         if not isinstance(middleware_callbacks, list):
             middleware_callbacks = [middleware_callbacks]
         kwargs = {
