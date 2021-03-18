838f94dafed0f9037ee755971cc7cdcec007f939
Richard Tibbles <richard@learningequality.org>
2021-03-18 08:35:01 -0700

    Add tests for macos and windows.

--- tests/test_videos.py
+++ tests/test_videos.py
@@ -148,6 +148,7 @@ def get_resolution(videopath):
     width, height = int(m.group(1)), int(m.group(2))
     return width, height
 
+@pytest.mark.skipif(os.name == 'nt', reason="Skipping ffmpeg tests on Windows.")
 class Test_video_compression(object):
 
     def setup_method(self):
@@ -196,7 +197,7 @@ class Test_video_compression(object):
 
 
 """ *********** TEST VIDEO CONVERSION  *********** """
-
+@pytest.mark.skipif(os.name == 'nt', reason="Skipping ffmpeg tests on Windows.")
 class Test_video_conversion(object):
 
     def setup_method(self):
