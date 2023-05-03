d5fbea3d2b1cf889321e90427c7679a62087266e
Richard Tibbles <richard@learningequality.org>
2023-05-03 09:54:22 -0700

    Update requirements configuration in setup.py. Remove unused dependencies.

--- setup.py
+++ setup.py
@@ -11,35 +11,6 @@ readme = open("README.md").read()
 with open("docs/history.rst") as history_file:
     history = history_file.read()
 
-requirements = [
-    "pytest>=3.0.2",
-    "requests>=2.11.1",
-    "le_utils>=0.1.26",
-    "validators",  # TODO: check if this is necessary
-    "requests_file",
-    "beautifulsoup4>=4.6.3,<4.9.0",  # pinned to match versions in le-pycaption
-    "selenium==3.0.1",
-    "yt-dlp>=2023.3.4",
-    "html5lib",
-    "cachecontrol==0.12.0",
-    "lockfile==0.12.2",  # TODO: check if this is necessary
-    "css-html-js-minify==2.2.2",
-    "mock==2.0.0",
-    "pypdf2==1.26.0",
-    "dictdiffer>=0.8.0",
-    "Pillow==8.4.0",
-    "colorlog>=4.1.0,<4.2",
-    "PyYAML>=5.3.1",
-    "Jinja2>=2.10",
-    "chardet==4.0.0",
-    "ffmpy>=0.2.2",
-    "pdf2image==1.11.0",
-    "le-pycaption>=2.2.0a1",
-    "EbookLib>=0.17.1",
-    "filetype>=1.1.0",
-]
-
-
 setup(
     name="ricecooker",
     version=ricecooker.__version__,
@@ -58,7 +29,30 @@ setup(
         ]
     },
     include_package_data=True,
-    install_requires=requirements,
+    install_requires=[
+        "requests>=2.11.1",
+        "le_utils>=0.1.26",
+        "requests_file",
+        "beautifulsoup4>=4.6.3,<4.9.0",  # pinned to match versions in le-pycaption
+        "selenium==3.0.1",
+        "yt-dlp>=2023.3.4",
+        "html5lib",
+        "cachecontrol==0.12.0",
+        "lockfile==0.12.2",  # This is needed, but not specified as a dependency by cachecontrol
+        "css-html-js-minify==2.2.2",
+        "pypdf2==1.26.0",
+        "dictdiffer>=0.8.0",
+        "Pillow==8.4.0",
+        "colorlog>=4.1.0,<4.2",
+        "PyYAML>=5.3.1",
+        "Jinja2>=2.10",
+        "chardet==4.0.0",
+        "ffmpy>=0.2.2",
+        "pdf2image==1.11.0",
+        "le-pycaption>=2.2.0a1",
+        "EbookLib>=0.17.1",
+        "filetype>=1.1.0",
+    ],
     python_requires=">=3.7, <3.11",
     license="MIT license",
     zip_safe=False,
