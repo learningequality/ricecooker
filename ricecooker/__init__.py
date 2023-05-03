73f87ac0d0f1a89b697187607d884f935e2e9689
Richard Tibbles <richard@learningequality.org>
2023-05-02 19:21:15 -0700

    Bump version

--- ricecooker/__init__.py
+++ ricecooker/__init__.py
@@ -2,10 +2,10 @@
 
 __author__ = "Learning Equality"
 __email__ = "info@learningequality.org"
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 
 import sys
 
-if sys.version_info < (3, 6, 0):
-    raise RuntimeError("Ricecooker only supports Python 3.6+")
+if sys.version_info < (3, 7, 0):
+    raise RuntimeError("Ricecooker only supports Python 3.7+")
